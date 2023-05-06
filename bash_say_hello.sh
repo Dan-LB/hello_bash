e96f8382ba04d1c428f999ba7b4bc227f19efc45
Dan-LB <98643865+Dan-LB@users.noreply.github.com>
2023-05-06 12:20:24 +0200

    Create bash_say_hello.sh

---
+++ bash_say_hello.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --gres=gpu:1
+#SBATCH --time=0:02:00
+#SBATCH --mem=4GB
+#SBATCH --error=myJob.err
+#SBATCH --output=myJob.out
+#SBATCH --account=IsCa5_SLQE
+
+env/myenv/bin/python from_github/write_hello.py 
