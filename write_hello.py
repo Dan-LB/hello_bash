394314f764bf3a4706084886ffb831dfb5e78a42
Dan-LB <98643865+Dan-LB@users.noreply.github.com>
2023-05-06 12:12:40 +0200

    Create write_hello.py

---
+++ write_hello.py
@@ -0,0 +1,12 @@
+import os
+
+
+print("Hello world.")
+
+# Create the results folder if it doesn't exist
+if not os.path.exists("results"):
+    os.mkdir("results")
+
+# Create a new file called output.txt in the results folder
+with open("results/output.txt", "w") as f:
+    f.write("Hello world, this is bash :)")
